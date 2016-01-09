7d5ccc10c76692c21fde8c1ac31acc046bafb039
Afaan Bilal <afaanbilal@gmail.com>
2016-01-09 11:39:28 +0530

    Added LCM

---
+++ lcm.c
@@ -0,0 +1,40 @@
+/*
+ *  Math Tools in C 
+ *      LCM
+ *
+ *  https://afaan.ml/math-tools-in-c 
+ * 
+ *  (c) Afaan Bilal (https://google.com/+AfaanBilal)
+ *
+ */
+ 
+ 
+#include <stdio.h>
+
+int main()
+{
+	int a, b, LCM;
+	
+    printf("Enter the first number  : ");
+    scanf("%i", &a);
+	
+    printf("Enter the second number : ");
+    scanf("%i", &b);
+
+	LCM = (a > b) ? a : b;
+
+    while (1)
+    {
+        if (LCM % a == 0 && LCM % b == 0)
+        {
+            printf("The LCM of %i and %i is %i", a, b, LCM);
+            
+            break;
+        }
+        else
+            LCM++; 
+    }
+    
+    getch();
+	return 0;
+}
