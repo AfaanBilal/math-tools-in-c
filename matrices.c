060512a25e51141a4349d12fc45dc6e3d9e65ea8
Afaan Bilal <afaanbilal@gmail.com>
2016-01-15 17:12:28 +0530

    Spacing corrections

--- matrices.c
+++ matrices.c
@@ -46,13 +46,13 @@ typedef struct
 void AllocateM(Matrix *M, int m, int n)
 {
     int i;
-    
-	M->m = m;
-	M->n = n;
-	M->E = malloc(m * sizeof(float)); //new double*[m];
-    
-	for (i = 0; i < m; i++)
-		M->E[i] = malloc(n * sizeof(float)); //new double[n];
+
+    M->m = m;
+    M->n = n;
+    M->E = malloc(m * sizeof(float)); //new double*[m];
+
+    for (i = 0; i < m; i++)
+        M->E[i] = malloc(n * sizeof(float)); //new double[n];
 }
 
 /// <signature>
@@ -63,13 +63,13 @@ void AllocateM(Matrix *M, int m, int n)
 void PrintMatrix(Matrix A)
 {
     int i, j;
-    
-	for (i = 0; i < A.m; i++) 
+
+    for (i = 0; i < A.m; i++) 
     {
-		for (j = 0; j < A.n; j++)
-			printf("%.1f\t", A.E[i][j]);
-		printf("\n");
-	}
+        for (j = 0; j < A.n; j++)
+            printf("%.1f\t", A.E[i][j]);
+        printf("\n");
+    }
 }
 
 /// <signature>
@@ -79,16 +79,16 @@ void PrintMatrix(Matrix A)
 /// </signature>
 void InputMatrix(Matrix *A)
 {
-	int i, j, m, n;
+    int i, j, m, n;
     printf("\nEnter the order: (m n) : ");
     scanf("%i %i", &m, &n);
-	
-	AllocateM(A, m, n);
 
-	printf("\nEnter the elements: \n");
-	for (i = 0; i < A->m; i++)
-		for (j = 0; j < A->n; j++)
-			scanf("%f", &(A->E[i][j]));
+    AllocateM(A, m, n);
+
+    printf("\nEnter the elements: \n");
+    for (i = 0; i < A->m; i++)
+        for (j = 0; j < A->n; j++)
+            scanf("%f", &(A->E[i][j]));
 }
 
 /// <signature>
@@ -100,16 +100,16 @@ void InputMatrix(Matrix *A)
 bool areEqual(Matrix A, Matrix B)
 {
     int i, j;
-    
-	if (A.m != B.m || A.n != B.n)
-		return false;
 
-	for (i = 0; i < A.m; i++)
-		for (j = 0; j < A.n; j++)
-			if (A.E[i][j] != B.E[i][j])
-				return false;
+    if (A.m != B.m || A.n != B.n)
+        return false;
 
-	return true;
+    for (i = 0; i < A.m; i++)
+        for (j = 0; j < A.n; j++)
+            if (A.E[i][j] != B.E[i][j])
+                return false;
+
+    return true;
 }
 
 /// <signature>
@@ -121,22 +121,22 @@ bool areEqual(Matrix A, Matrix B)
 Matrix Add(Matrix A, Matrix B)
 {
     int i, j;
-	Matrix R;
-    
-	if (A.m != B.m || A.n != B.n)
-	{
-		Matrix x;
-		x.m = x.n = 0;
-		return x;
-	}
-	
-	AllocateM(&R, A.m, A.n);
+    Matrix R;
 
-	for (i = 0; i < A.m; i++)
-		for (j = 0; j < A.n; j++)
-			R.E[i][j] = A.E[i][j] + B.E[i][j];
+    if (A.m != B.m || A.n != B.n)
+    {
+        Matrix x;
+        x.m = x.n = 0;
+        return x;
+    }
 
-	return R;
+    AllocateM(&R, A.m, A.n);
+
+    for (i = 0; i < A.m; i++)
+        for (j = 0; j < A.n; j++)
+            R.E[i][j] = A.E[i][j] + B.E[i][j];
+
+    return R;
 }
 
 /// <signature>
@@ -149,21 +149,21 @@ Matrix Subtract(Matrix A, Matrix B)
 {
     int i, j;
     Matrix R;
-	
-	if (A.m != B.m || A.n != B.n)
-	{
-		Matrix x;
-		x.m = x.n = 0;
-		return x;
-	}
 
-	AllocateM(&R, A.m, A.n);
+    if (A.m != B.m || A.n != B.n)
+    {
+        Matrix x;
+        x.m = x.n = 0;
+        return x;
+    }
 
-	for (i = 0; i < A.m; i++)
-		for (j = 0; j < A.n; j++)
-			R.E[i][j] = A.E[i][j] - B.E[i][j];
+    AllocateM(&R, A.m, A.n);
 
-	return R;
+    for (i = 0; i < A.m; i++)
+        for (j = 0; j < A.n; j++)
+            R.E[i][j] = A.E[i][j] - B.E[i][j];
+
+    return R;
 }
 
 /// <signature>
@@ -175,27 +175,27 @@ Matrix Subtract(Matrix A, Matrix B)
 Matrix Multiply(Matrix A, Matrix B)
 {
     Matrix R;
-	int i, j, k;
-    
-	if (A.n != B.m)
-	{
-		Matrix x;
-		x.m = x.n = 0;
-		return x;
-	}
-	
-	AllocateM(&R, A.m, B.n);
+    int i, j, k;
 
-	for (i = 0; i < R.m; i++)
-		for (j = 0; j < R.n; j++)
-			R.E[i][j] = 0;
+    if (A.n != B.m)
+    {
+        Matrix x;
+        x.m = x.n = 0;
+        return x;
+    }
 
-	for (i = 0; i < R.m; i++)
-		for (j = 0; j < R.n; j++)
-			for (k = 0; k < A.n; k++)
-				R.E[i][j] += A.E[i][k] * B.E[k][j];
+    AllocateM(&R, A.m, B.n);
 
-	return R;
+    for (i = 0; i < R.m; i++)
+        for (j = 0; j < R.n; j++)
+            R.E[i][j] = 0;
+
+    for (i = 0; i < R.m; i++)
+        for (j = 0; j < R.n; j++)
+            for (k = 0; k < A.n; k++)
+                R.E[i][j] += A.E[i][k] * B.E[k][j];
+
+    return R;
 }
 
 /// <signature>
@@ -206,16 +206,16 @@ Matrix Multiply(Matrix A, Matrix B)
 /// </signature>
 Matrix MultiplyK(Matrix A, float k)
 {
-	Matrix R;
+    Matrix R;
     int i, j;
-    
-	AllocateM(&R, A.m, A.n);
 
-	for (i = 0; i < R.m; i++)
-		for (j = 0; j < R.n; j++)
-			R.E[i][j] = k * A.E[i][j];
+    AllocateM(&R, A.m, A.n);
 
-	return R;
+    for (i = 0; i < R.m; i++)
+        for (j = 0; j < R.n; j++)
+            R.E[i][j] = k * A.E[i][j];
+
+    return R;
 }
 
 /// <signature>
@@ -225,16 +225,16 @@ Matrix MultiplyK(Matrix A, float k)
 /// </signature>
 Matrix Transpose(Matrix A)
 {
-	Matrix R;
-	int i, j;
-    
-    AllocateM(&R, A.n, A.m);
-    
-	for (i = 0; i < R.m; i++)
-		for (j = 0; j < R.n; j++)
-			R.E[i][j] = A.E[j][i];
+    Matrix R;
+    int i, j;
 
-	return R;
+    AllocateM(&R, A.n, A.m);
+
+    for (i = 0; i < R.m; i++)
+        for (j = 0; j < R.n; j++)
+            R.E[i][j] = A.E[j][i];
+
+    return R;
 }
 
 /// <signature>
@@ -245,112 +245,112 @@ Matrix Transpose(Matrix A)
 /// </signature>
 float Determinant(Matrix A, int n)
 {
-	float det = 0;
-	int p, h, k, i, j;
-	Matrix temp;
-	
+    float det = 0;
+    int p, h, k, i, j;
+    Matrix temp;
+
     if (A.m != A.n)
-		return -2.718;
+        return -2.718;
 
-	AllocateM(&temp, n, n);
+    AllocateM(&temp, n, n);
 
-	if (n == 1)
-		return A.E[0][0];
-	else if (n == 2)
-	{
-		det = (A.E[0][0] * A.E[1][1]) - (A.E[1][0] * A.E[0][1]);
-		return det;
-	}
-	else
-	{
-		for (p = 0; p < n; p++)
-		{
-			h = 0; k = 0;
-			for (i = 1; i < n; i++)
-			{
-				for (j = 0; j < n; j++)
-				{
-					if (j == p)
-						continue;
-					temp.E[h][k] = A.E[i][j];
-					k++;
-					if (k == n - 1)
-					{
-						h++;
-						k = 0; 
-					}
-				}
-			}
+    if (n == 1)
+        return A.E[0][0];
+    else if (n == 2)
+    {
+        det = (A.E[0][0] * A.E[1][1]) - (A.E[1][0] * A.E[0][1]);
+        return det;
+    }
+    else
+    {
+        for (p = 0; p < n; p++)
+        {
+            h = 0; k = 0;
+            for (i = 1; i < n; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    if (j == p)
+                        continue;
+                    temp.E[h][k] = A.E[i][j];
+                    k++;
+                    if (k == n - 1)
+                    {
+                        h++;
+                        k = 0; 
+                    }
+                }
+            }
 
-			det += A.E[0][p] * pow(-1, p) * Determinant(temp, n - 1);
-		}
+            det += A.E[0][p] * pow(-1, p) * Determinant(temp, n - 1);
+        }
 
-		return det;
-	}
+        return det;
+    }
 }
 
 
 int main()
 {
-	printf("Matrix M1: ");
-	Matrix M1;
-	InputMatrix(&M1);
+    printf("Matrix M1: ");
+    Matrix M1;
+    InputMatrix(&M1);
 
-	printf("\nMatrix M2: ");
-	Matrix M2;
-	InputMatrix(&M2);
-	
-	printf("\nDeterminant (M1): ");
-	float d1 = Determinant(M1, M1.m);
-	if (M1.m != M1.n)
-		printf("M1 is not a square matrix.\n");
-	else if (d1 == 0)
-		printf("M1 is a singluar matrix (determinant equal to zero).\n");
-	else
-		printf("M1 is a non-singluar matrix with determinant equal to %f \n", d1);
+    printf("\nMatrix M2: ");
+    Matrix M2;
+    InputMatrix(&M2);
 
-	printf("\nDeterminant (M2): ");
-	float d2 = Determinant(M2, M2.m);
-	if (M2.m != M2.n)
-		printf("M2 is not a square matrix.\n");
-	else if (d2 == 0)
-		printf("M2 is a singluar matrix (determinant equal to zero).\n");
-	else
-		printf("M2 is a non-singluar matrix with determinant equal to %f \n", d2);
+    printf("\nDeterminant (M1): ");
+    float d1 = Determinant(M1, M1.m);
+    if (M1.m != M1.n)
+        printf("M1 is not a square matrix.\n");
+    else if (d1 == 0)
+        printf("M1 is a singluar matrix (determinant equal to zero).\n");
+    else
+        printf("M1 is a non-singluar matrix with determinant equal to %f \n", d1);
 
-	printf("\nMultiplication (M1 * k) where k = 5: \n");
-	PrintMatrix(MultiplyK(M1, 5));
+    printf("\nDeterminant (M2): ");
+    float d2 = Determinant(M2, M2.m);
+    if (M2.m != M2.n)
+        printf("M2 is not a square matrix.\n");
+    else if (d2 == 0)
+        printf("M2 is a singluar matrix (determinant equal to zero).\n");
+    else
+        printf("M2 is a non-singluar matrix with determinant equal to %f \n", d2);
 
-	printf("\nTranspose (M1): \n");
-	PrintMatrix(Transpose(M1));
-	
-	printf("\nTranspose (M2): \n");
-	PrintMatrix(Transpose(M2));
+    printf("\nMultiplication (M1 * k) where k = 5: \n");
+    PrintMatrix(MultiplyK(M1, 5));
 
-	printf("\nAddition (M1 + M2): \n");
-	Matrix Sum = Add(M1, M2);
-	if (Sum.m == 0)
-		printf(" Matrices M1 and M2 don't have the same order. \n");
-	else {
-		PrintMatrix(Sum);
-	}
+    printf("\nTranspose (M1): \n");
+    PrintMatrix(Transpose(M1));
 
-	printf("\nSubtraction (M1 - M2): \n");
-	Matrix Dif = Subtract(M1, M2);
-	if (Dif.m == 0)
-		printf(" Matrices M1 and M2 don't have the same order. \n");
-	else {
-		PrintMatrix(Dif);
-	}
+    printf("\nTranspose (M2): \n");
+    PrintMatrix(Transpose(M2));
 
-	printf("\nMultiplication (M1 x M2): \n");
-	Matrix P = Multiply(M1, M2);
-	if (P.m == 0)
-		printf(" Multiplication of matrices M1 and M2 is not defined.\n(M1.n should be equal to M2.m)\n");
-	else {
-		PrintMatrix(P);
-	}
+    printf("\nAddition (M1 + M2): \n");
+    Matrix Sum = Add(M1, M2);
+    if (Sum.m == 0)
+        printf(" Matrices M1 and M2 don't have the same order. \n");
+    else {
+        PrintMatrix(Sum);
+    }
 
-	getch();
+    printf("\nSubtraction (M1 - M2): \n");
+    Matrix Dif = Subtract(M1, M2);
+    if (Dif.m == 0)
+        printf(" Matrices M1 and M2 don't have the same order. \n");
+    else {
+        PrintMatrix(Dif);
+    }
+
+    printf("\nMultiplication (M1 x M2): \n");
+    Matrix P = Multiply(M1, M2);
+    if (P.m == 0)
+        printf(" Multiplication of matrices M1 and M2 is not defined.\n(M1.n should be equal to M2.m)\n");
+    else {
+        PrintMatrix(P);
+    }
+
+    getch();
     return 0;
 }
