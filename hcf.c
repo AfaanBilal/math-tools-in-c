0546f646625e9605652c5254538e200eb9da3a9a
Afaan Bilal <afaanbilal@gmail.com>
2016-01-17 18:19:02 +0530

    Switched to shorter recursive algorithm

--- hcf.c
+++ hcf.c
@@ -11,48 +11,22 @@
  
 #include <stdio.h>
 
+int hcf(int a, int b)
+{
+    return (b == 0) ? a : hcf(b, a % b);
+}
+
 int main()
 {
-    int a, b, r, orgA, orgB, HCF;
+    int a, b;
 
     printf("Enter the first number  : ");
-    scanf("%i", &orgA);
+    scanf("%i", &a);
 
     printf("Enter the second number : ");
-    scanf("%i", &orgB);
-
-    if (orgA > orgB)
-    {
-        a = orgA;
-        b = orgB;
-    }
-    else
-    {
-        a = orgB;
-        b = orgA;
-    }
-
-    //Euclids Lemma: a = b * q + r where 0 <= r < q; 
-
-    while (b >= 1)
-    {
-        if (a % b == 0)
-        {
-            HCF = b;
-            printf("The HCF of %i and %i is %i", orgA, orgB, HCF);
-            
-            if (HCF == 1)
-                printf("\nThe numbers are co-prime");
-            
-            break;
-        }
-        else
-        {
-            r = a % b;
-            a = b;
-            b = r;
-        }    
-    }
+    scanf("%i", &b);
+    
+    printf("HCF of %i and %i is %i", a, b, hcf(a, b));
 
     getch();
     return 0;
