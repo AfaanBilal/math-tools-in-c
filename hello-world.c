5bb8dcc5bbf3c4633a2e451ddb0829a244cafd43
Afaan Bilal <afaanbilal@gmail.com>
2023-03-12 19:26:57 +0530

    ✨ #define Hello World

---
+++ hello-world.c
@@ -0,0 +1,19 @@
+/*
+ *  Math Tools in C
+ *      Hello World
+ *
+ *  https://afaan.dev/math-tools-in-c
+ *
+ *  (c) Afaan Bilal (https://afaan.dev)
+ *
+ */
+
+#include <stdio.h>
+
+#define hello "Hello, "
+#define world "world!\n"
+
+int main()
+{
+    printf(hello world);
+}
